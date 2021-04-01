f2a863bcf391af3cdd37379a1138a453be2de636
peamexx <peamexx@gmail.com>
2021-04-01 18:35:35 +0900

    [ADD] login.html 제작

---
+++ script/script.js
@@ -0,0 +1,16 @@
+let requiredY = document.querySelectorAll('.requiredY');
+let loginBtn = document.querySelector('.loginBtn');
+
+loginBtn.addEventListener('click', requireChk);
+
+// 필수 입력 요소 체크
+function requireChk() {
+    requiredY.forEach((item) => {
+        if(item.value === '') {
+            let title = item.closest('div').querySelector('label').textContent;
+            
+            item.closest('div').querySelector('.error').classList.add('on');
+            item.closest('div').querySelector('.error').textContent = `${title}를 입력해주세요`;
+        }
+    });
+};
