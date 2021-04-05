921b583b4169e527536a6206a049cd8236c2f963
peamexx <peamexx@gmail.com>
2021-04-05 18:17:44 +0900

    [UPDATE] 이벤트 조건 click ---> blur 변경 및 비번 조건 변경

--- join/script.js
+++ join/script.js
@@ -1,40 +1,38 @@
 let idInput = document.querySelector('.idArea').querySelector('input');
 let pwInput = document.querySelector('.pwArea').querySelector('input');
-let pwChkAreaInput = document.querySelector('.pwChkArea').querySelector('input');
-let phoneNumAreaInput = document.querySelector('.phoneNumArea').querySelector('input');
+let pwChkInput = document.querySelector('.pwChkArea').querySelector('input');
+let phoneNumInput = document.querySelector('.phoneNumArea').querySelector('input');
 
-idInput.addEventListener('keyup', pwChkFunc);
-pwChkAreaInput.addEventListener('keyup', pwChkFunc);
-phoneNumAreaInput.addEventListener('keyup', phoneNumFunc);
+// idInput.addEventListener('blur', pwChkFunc);
+pwChkInput.addEventListener('blur', pwChkFunc);
+phoneNumInput.addEventListener('blur', phoneNumFunc);
 
 // https://regexr.com/
 
 // 아이디 체크
-// 만드셈
+// 나중에 하단 버튼 만들고 해야함
 
 // 비밀번호 확인 체크
-// 오류난 후 글자 지우고 아예 창 빠져나갔을때도 처리 따로 해줘야함
 function pwChkFunc() {
-    let pwChkAreaError = document.querySelector('.pwChkArea').querySelector('.error');
+    let pwChkError = document.querySelector('.pwChkArea').querySelector('.error');
 
-    pwChkAreaError.textContent = "비밀번호가 맞지 않습니다.";
-    pwChkAreaError.classList.add('on');
+    pwChkError.textContent = "비밀번호가 맞지 않습니다.";
+    pwChkError.classList.add('on');
 
-    if(pwInput.value === pwChkAreaInput.value) {
-        pwChkAreaError.classList.remove('on');
+    if(pwInput.value === pwChkInput.value) {
+        pwChkError.classList.remove('on');
     };
 };
 
 // 휴대폰번호 체크
-// 오류난 후 글자 지우고 아예 창 빠져나갔을때도 처리 따로 해줘야함
 function phoneNumFunc() {
-    let phoneNumAreaError = document.querySelector('.phoneNumArea').querySelector('.error');
-    let onlyNum = /^[0-9]+$/;
+    let phoneNumError = document.querySelector('.phoneNumArea').querySelector('.error');
+    let onlyNum = /^01[0-9]{8,9}$/; // 숫자만, 010/011.. 지원, 010-123-4567 지원
 
-    if(!phoneNumAreaInput.value.match(onlyNum)) {
-        phoneNumAreaError.classList.add('on');
-        phoneNumAreaError.textContent = "숫자만 입력해주세요.";
+    if(!phoneNumInput.value.match(onlyNum)) {
+        phoneNumError.classList.add('on');
+        phoneNumError.textContent = "숫자만 입력해주세요.";
     } else {
-        phoneNumAreaError.classList.remove('on');
+        phoneNumError.classList.remove('on');
     }
 };
