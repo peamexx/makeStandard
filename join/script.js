16d470f2ab7e53706943209a3899a14909a26066
peamexx <peamexx@gmail.com>
2021-04-06 15:52:26 +0900

    [UPDATE] 비밀번호 체크 제작

--- join/script.js
+++ join/script.js
@@ -4,6 +4,7 @@ let pwChkInput = document.querySelector('.pwChkArea').querySelector('input');
 let phoneNumInput = document.querySelector('.phoneNumArea').querySelector('input');
 
 // idInput.addEventListener('blur', pwChkFunc);
+pwInput.addEventListener('blur', pwFunc);
 pwChkInput.addEventListener('blur', pwChkFunc);
 phoneNumInput.addEventListener('blur', phoneNumFunc);
 
@@ -12,11 +13,26 @@ phoneNumInput.addEventListener('blur', phoneNumFunc);
 // 아이디 체크
 // 나중에 하단 버튼 만들고 해야함
 
+// 비밀번호 체크
+function pwFunc() {
+    let pwError = document.querySelector('.pwArea').querySelector('.error');
+    let pwCondition = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*\W).{8,10}$/;
+    let pwConditionSymbol = ['!','.',',','"',"'"];
+
+    if(!pwInput.value.match(pwCondition) ||
+        pwInput.value.match(pwConditionSymbol)) {
+        pwError.classList.add('on');
+        pwError.textContent = '비밀번호 조건에 맞지 않습니다.';
+    } else {
+        pwError.classList.remove('on');
+    }
+};
+
 // 비밀번호 확인 체크
 function pwChkFunc() {
     let pwChkError = document.querySelector('.pwChkArea').querySelector('.error');
 
-    pwChkError.textContent = "비밀번호가 맞지 않습니다.";
+    pwChkError.textContent = '비밀번호가 맞지 않습니다.';
     pwChkError.classList.add('on');
 
     if(pwInput.value === pwChkInput.value) {
@@ -27,11 +43,11 @@ function pwChkFunc() {
 // 휴대폰번호 체크
 function phoneNumFunc() {
     let phoneNumError = document.querySelector('.phoneNumArea').querySelector('.error');
-    let onlyNum = /^01[0-9]{8,9}$/; // 숫자만, 010/011.. 지원, 010-123-4567 지원
+    let phoneNumCondition = /^01[0-9]{8,9}$/; // 숫자만, 010/011.. 지원, 010-123-4567 지원
 
-    if(!phoneNumInput.value.match(onlyNum)) {
+    if(!phoneNumInput.value.match(phoneNumCondition)) {
         phoneNumError.classList.add('on');
-        phoneNumError.textContent = "숫자만 입력해주세요.";
+        phoneNumError.textContent = '숫자만 입력해주세요.';
     } else {
         phoneNumError.classList.remove('on');
     }
